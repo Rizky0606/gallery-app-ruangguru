35c388bd2354cfc68892cd7b3008078cf63dfa3a
Rizky0606 <fauzirizky461@gmail.com>
2023-06-16 17:53:32 +0700

    init commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Rizky Fauzi Ardiansyah"; // TODO: replace this
+const studentId = "FE5393983"; // TODO: replace this
+
+export { url, studentName, studentId };
